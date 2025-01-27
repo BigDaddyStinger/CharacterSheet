abf7bb3eea46e95dd1ce8a4fd91efa02f900c873
BigDaddyStinger <mattwalsh5697@gmail.com>
2025-01-27 14:30:48 -0600

    PreCompletionSetup
    Setting up the repository prior to completing the week 2 script assignment.

---
+++ Assets/CharacterSheet.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CharacterSheet : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        string characterName;
+        int profBonus;
+        bool finesseWeapon;
+        int strengthBonus;
+        int dexBonus;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
